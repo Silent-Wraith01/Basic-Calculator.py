2d909e0df5e8802e58b6e3e9a648e60d226c5414
Silent-Wraith01 <gamer20@live.com>
2026-05-06 15:49:06 -0500

    Add files via upload

---
+++ Regular practice.py
@@ -0,0 +1,18 @@
+operator = input("Enter the operator (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result))  
+elif operator == "-":
+    result = num1 - num2
+    print(round(result)) 
+elif operator == "*":
+    result = num1 * num2
+    print(round(result)) 
+elif operator == "/":
+    result = num1 / num2
+    print(round(result)) 
+else:
+    print(f"Invalid operator: {operator}") 
